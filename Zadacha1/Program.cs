5079fd1e77f8cb1c1cf5376f9d7a08d04478356e
wsxfvhu <krmiv77@gmail.com>
2022-01-09 20:10:34 +0300

    Добавьте файлы проекта.

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> brack = new Stack<string>();
+            string brackets = "(kjjlkj)[{kjhj}]   lk;lk()";
+            foreach (char arg in brackets)
+            {
+                string argStr = arg.ToString();
+                switch (argStr)
+                {
+                    case "(":
+                        brack.Push(")"); break;
+                    case "[":
+                        brack.Push("]"); break;
+                    case "{":
+                        brack.Push("}"); break;
+                    case ")":
+                        if (brack.Contains(")"))
+                        {
+                            if (argStr == brack.Peek())
+                            {
+                                argStr = brack.Pop();
+                                break;
+                            }
+                            else
+                            {
+                                brack.Push(")");
+                            }
+                        }
+                        else
+                        {
+                            brack.Push(")");
+                        }
+                        break;
+                    case "]":
+                        if (brack.Contains("]"))
+                        {
+                            if (argStr == brack.Peek())
+                            {
+                                argStr = brack.Pop();
+                                break;
+                            }
+                            else
+                            {
+                                brack.Push("]");
+                            }
+                        }
+                        else
+                        {
+                            brack.Push("]");
+                        }
+                        break;
+                    case "}":
+                        if (brack.Contains("}"))
+                        {
+                            if (argStr == brack.Peek())
+                            {
+                                argStr = brack.Pop();
+                                break;
+                            }
+                            else
+                            {
+                                brack.Push("}");
+                            }
+                        }
+                        else
+                        {
+                            brack.Push("}");
+                        }
+                        break;
+                }
+            }
+            if (brack.Count == 0)
+            {
+                Console.WriteLine("Скобки расставлены корректно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расставлены не корректно");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
